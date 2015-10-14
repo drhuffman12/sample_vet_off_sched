02aa60368b6e7737a161079a5541d941ae00eb5d
Daniel Huffman <drhuffman12@yahoo.com>
2015-10-14 15:25:11 -0400

    add simple menu (security to be added later)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :prep_menu
+
+  def prep_menu
+    @menu = []
+    @menu << {label: 'Roles', link: roles_path}
+    @menu << {label: 'Users', link: users_path}
+    @menu << {label: 'Pets', link: pets_path}
+    @menu << {label: 'Appointments', link: appointments_path}
+  end
 end
